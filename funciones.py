ab2de7147b3a79a88641f90301a7c0cdeb7ecb7b
David <davicolo717@gmail.com>
2025-07-10 13:41:30 -0400

    en proceso

---
+++ funciones.py
@@ -0,0 +1,9 @@
+
+def menu():
+    print("\n--Bienvenido a Pybooks--")
+    print("---Menu Principal---")
+    print("1) Stock por marca.")
+    print("2) Busqueda por precio.")
+    print("3) Actualizar precio.")
+    print("4) Salir.")
+
--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+from funciones import menu
+
+productos = {
+    '001HD': ['HP', 15.6, '8GB', 'DD', '1TB', 'Intel Core i5', 'Nvidia GTX1050'],
+    '002HD': ['Lenovo', 14, '4GB', 'SSD', '512GB', 'Intel Core i5', 'Nvidia GTX1050'],
+    '003HD': ['ASUS', 14, '16GB', 'SSD', '256GB', 'Intel Core i7', 'Nvidia RTX2080Ti'],
+    '004HD': ['HP', 15.6, '8GB', 'DD', '1TB', 'Intel Core i3', 'Graficos Integrados'],
+    '005HD': ['MSI', 17, '32GB', 'NVME M.2', '2TB', 'AMD Ryzen 9', 'Nvidia RTX5090']
+}
+
+stock = {
+    '001HD': [449990,20], '002HD': [299990,20],
+    '003HD': [679990,10], '004HD': [399990,20],'005':[1199990,5]
+}
